7fd9d9cc151a3a3788d88a642cc5fe504d75c1c3
Mor Kasus <morkasus3@gmail.com>
2016-06-14 12:11:59 +0300

    s

--- statistics/index.js
+++ statistics/index.js
@@ -9,7 +9,8 @@ module.exports = class Statistics {
     showAchievements(userName,callback) {
     	User.findOne({ 'userName': userName}, function (err, user) {
             console.log(user);
-  			if (err || (user == null)) callback({});
+  			if (err) callback({});
+            if(user == null) callback({});
   			//contain win count. 
   			var userWin = user.wins;
   			//contain losses count.
